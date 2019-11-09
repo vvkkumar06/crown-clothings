5a599edccaec417e4f7d8245d61f1d587d451a2d
Vivek Kumar <vivek.kumar@optimusinfo.com>
2019-11-09 11:48:13 +0530

    broken our homepage into smaller componente and finished styling our menu items

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,40 +1,10 @@
 import React from 'react';
 import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component';
 
 const HomePage = () => {
     return (<div className='homepage'>
-        <div className='directory-menu'>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>Hats</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>Jackets</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>Sneakers</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>Mens</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>Womens</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-        </div>
+        <Directory />
     </div>);
 }
 
